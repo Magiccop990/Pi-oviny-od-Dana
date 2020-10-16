7ee23bd64841c2f34c935c29805d74641ad7b0dd
Streamer272 <70911709+Streamer272@users.noreply.github.com>
2020-10-16 07:38:47 +0200

    Clicker_io.py
    Update. Not tested yet

--- clicker_io.py
+++ clicker_io.py
@@ -1,12 +1,13 @@
 from tkinter import *
 from pynput.keyboard import Listener
 import time
+import tkinter as tk
 
 
 print("press SPACE to earn")
 cash = 0
 
-class File():
+class File(file):
     def filename_get(self, filename):
         self.filename = filename
     def File_write(self):
@@ -23,8 +24,23 @@ class File():
         print("Current cash: " + str(cash))
         file_read.close()
 
+class Window(ui):
+    def __init__(self):
+        cash = self.money
+        win = Tk()
+        win.title("CLICKER.IO")
+        win.geometry("200x100")
+        main_label = Label(win, text=cash, width=5)
+        main_label.place(x=90,y=10)
+        main_frame = LabelFrame(win, width=5, height=2)
+        main_frame.place(x=90, y=10)
+        
+    def main_update(self):
+        cash = self.money
+        main_label.configurate(text=cash)
 
-class Keys_get():
+
+class Keys(listener_commands):
     def Get_cash(self, useless):
         File.filename_get(self=self, filename="clicker_io")
         File.File_read(self=self)
@@ -36,6 +52,7 @@ class Keys_get():
         if key == key.space:
             print("cash +1")
             cash += 1
+            Window.main_update(self)
             #print(cash)
         elif key == key.esc:
             File.File_write(self=self)
@@ -50,7 +67,6 @@ class Keys_get():
         Keys_get.release(self=self, key=key)
 
 
-Keys = Keys_get()
 Keys.Get_cash(useless=None)
 
 
