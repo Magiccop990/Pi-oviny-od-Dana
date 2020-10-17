f193e1ce50d72fdb137d66a65dfbcdbac6f67e23
Streamer272 <70911709+Streamer272@users.noreply.github.com>
2020-10-17 17:14:25 +0200

    clicker_io.py
    first working clicking game...

--- clicker_io.py
+++ clicker_io.py
@@ -8,6 +8,7 @@ import tkinter as tk
 Level = 0
 upgrade_list = ["McDonalds", "KFC", "Subway"]
 upgrades = {upgrade_list[0]:1, upgrade_list[1]:2, upgrade_list[2]:5}
+upgrades_cost = {upgrade_list[0]:500, upgrade_list[1]:1500, upgrade_list[2]:5000}
 income = upgrades[upgrade_list[Level]]
 
 #variables
@@ -16,13 +17,13 @@ num = 0
 key = str("")
 
 #info
-print("press SPACE to earn\npress ESC to exit\npress CTRL to upgrade")
+print("press SPACE to earn\npress ESC to exit\npress TAB to upgrade")
 
 
 #main class
 class main():
     def __init__(self, filename, cash, useless, key):
-        global income, upgrade_list, upgrades, Level
+        global income, upgrade_list, upgrades, Level, upgrades_cost
         self.upgrade_list = upgrade_list
         self.upgrades = upgrades
         self.Level = Level
@@ -30,6 +31,7 @@ class main():
         self.filename = filename
         self.cash = cash
         self.key = key
+        self.upgrades_cost = upgrades_cost
         main.Get_cash(self)
 
     #####FILE
@@ -48,17 +50,32 @@ class main():
         cash = int(self.file_o.readline())
         self.file_o.close()
         self.cash = cash
-        print("Current cash: " + str(cash))
+        print(f"Current cash: {str(cash)} €")
         
-        #window
+        #UPGRADES
 
-    def Windower(self):
-        cash = self.cash
-        upgrade_list = self.upgrade_list
-        upgrades = self.upgrades
+    def PU(self):
+        upgrades_cost = self.upgrades_cost
+        income = self.income
         Level = self.Level
-        #return 
-
+        cash = self.cash
+        try:
+            randvar = upgrades[upgrade_list[Level + 1]]
+        except:
+            print("Wait... You already have all upgrades.")
+        else:
+            if cash >= upgrades_cost[upgrade_list[Level]]:
+                print(f"This transaction cost you: {str(upgrades_cost[upgrade_list[Level]])} €")
+                cash -= upgrades_cost[upgrade_list[Level]]
+                print(f"You now have {cash} €")
+                Level += 1
+                income = upgrades[upgrade_list[Level]]
+                print(f"Your income is now: {income} €")
+                self.Level = Level
+                self.cash = cash
+                self.income = income
+            else:
+                print(f"You dont have enought money to buy this. Cost({str(upgrades_cost[upgrade_list[Level]])} €)")
 
     def Get_cash(self):
         main.File_read(self=self)
@@ -81,8 +98,21 @@ class main():
             print(f"You now have: {cash} €")
             exit()
 
-        elif key == key.ctrl_l:
-            main.Windower(self)
+        elif key == key.alt_l:
+            print(f"Upgrade names:money {upgrade_list}")
+
+        elif key == key.tab:
+            print("Searching for better business...")
+            time.sleep(1)
+            main.PU(self)
+
+        else:
+            print(f"Sorry, this key ({key}) doesnt do anything")
+
+    def search_by_val(self, val):
+        for shop in self.upgrade_list:
+            if val == self.names[shop]:
+                return shop
 
     def release(self, key):
         income = self.income
@@ -101,8 +131,11 @@ class main():
 
 Keys = main(filename="clicker_io", cash=cash, key=key, useless=None)
 
-"""
+def Main(bookshelf):
+    print(bookshelf)
+
+
 if __name__ == 'main':
-    Main()"""
+    Main("This is BOOKSHELF")
 
 Keys.Listener_join()
