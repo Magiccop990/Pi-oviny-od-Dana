4abeef6a264ed858f44b862fd0377397da90a719
Streamer272 <70911709+Streamer272@users.noreply.github.com>
2020-10-30 19:27:23 +0100

    Delete scriptSI.js

--- scriptSI.js
+++
@@ -1,7 +0,0 @@
-function OnClick() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    var button = document.getElementById("button");
-    alert("You Signed In as: " + username + " with password: " + password);
-    window.location = "index2SI.html"
-}
