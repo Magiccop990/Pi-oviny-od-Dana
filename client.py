a048eb71f274044c445cb2a6d9931e92c48903df
Streamer272 <70911709+Streamer272@users.noreply.github.com>
2020-11-01 18:38:56 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,30 +1,50 @@
-import socket, threading
+import socket, threading, time
 
-frequency = 5050  #frequency
-FORMAT = "utf-8"
-DISCONNECT_MSG = "!disconnect"
-ip = "192.168.1.21"# - "95.103.201.58" / "192.168.1.21"
-IP_PORT = (ip, frequency)
+class Online():
+    def __init__(self):
+        self.frequency = 1234
+        self.FORMAT = "utf-8"
+        self.DISCONNECT_MSG = "!d"
+        self.ip = "192.168.1.21"# - "95.103.201.58" / "192.168.1.21"
+        self.IP_PORT = (self.ip, self.frequency)
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.rcv = True
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(IP_PORT)
+    def Connect(self):
+        try:
+            self.client.connect(self.IP_PORT)
+        except:
+            print("The server isn't available...")
+            exit()
+        else:
+            pass
 
-def rcv():
-    rcv_msg = client.recv(2048).decode(FORMAT)
-    rcv_msg = rcv_msg.replace("[", "")
-    rcv_msg = rcv_msg.replace("]", "")
-    print(f"Chat: {rcv_msg}")
+    def rcv(self):
+        while self.rcv:
+            self.rcv_msg = self.client.recv(2048).decode(self.FORMAT)
+            self.rcv_msg = self.rcv_msg.replace("[", "")
+            self.rcv_msg = self.rcv_msg.replace("]", "")
+            self.rcv_msg = self.rcv_msg.replace("'", "")
+            print(f"Friend: {self.rcv_msg}")
+            time.sleep(1)
 
-def send(msg):
-    message_for_server = msg.encode(FORMAT)
-    client.send(message_for_server)
+    def send(self, msg):
+        self.message_for_server = self.msg.encode(FORMAT)
+        self.client.send(self.message_for_server)
 
-while True:
-    rcv_thread = threading.Thread(target=rcv)
-    rcv_thread.start()
-    to_send = str(input(""))
-    if to_send == "!disconnect":
-        send(to_send)
-        break
-    else:
-        send(to_send)
+    def main(self):
+        cisco.Connect()
+        self.rcv_thread = threading.Thread(target=cisco.rcv)
+        self.rcv_thread.start()
+        while True:
+            self.to_send = str(input(""))
+            if self.to_send == self.DISCONNECT_MSG:
+                cisco.send("!disconnect")
+                break
+            else:
+                cisco.send(self.to_send)
+
+cisco = Online()
+
+if __name__ == "__main__":
+    cisco.main()
